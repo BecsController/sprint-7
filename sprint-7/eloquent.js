0cae858f77df500c946dc824b1eb0dcad170b183
RebDugNZ <bec.dugdale16@gmail.com>
2018-02-26 13:47:57 +1300

    chap 3 exercises

---
+++ sprint-7/eloquent.js
@@ -0,0 +1,23 @@
+Minimum
+
+function min(first, second){
+  if (first < second)
+  return first;
+  else if (first > second)
+  return second;
+}
+
+console.log(min(1, 3));
+
+Recursion
+
+function isEven(number){
+  if (number )
+  return false;
+  else if ()
+  return true;
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
